7a72e4bc73e7447ee5ef2b219b7679c634a8c63f
marcsgil <marcosgildeoliveira@gmail.com>
2023-07-14 14:53:56 -0300

    made new plots

--- Code/Systems/Morse/critical_time.jl
+++ Code/Systems/Morse/critical_time.jl
@@ -1,14 +1,11 @@
-using Plots,LaTeXStrings
+include("../../plot_config.jl")
 
-default()
-default(label=false,width=4, markersize = 4, msw=0, 
-palette=:Set1_3, tickfontsize=14,
-guidefontsize=16, fontfamily="Computer Modern",dpi=1000,grid=false,framestyle = :box)
+θc(ϵ) = log(1 / √ϵ + √(1 / ϵ - 1)) / √(1 - ϵ)
 
-θc(ϵ) = log(1/√ϵ+√(1/ϵ-1))/√(1-ϵ)
-
-ϵs = LinRange(.001,1,1024)
-p = plot(ϵs,θc.(ϵs),xlabel=L"\epsilon",ylabel=L"\omega s_c",
-xlims=(0,1),ylims=(.9,3.1),size=(600,400),dpi=300,left_margin=2Plots.mm,bottom_margin=4Plots.mm)
+ϵs = LinRange(0.001, 1, 1024)
+f = Figure()
+ax = Axis(f[1, 1], xticks=0:0.2:1, xlabel=L"\epsilon", ylabel=L"\omega s_c")
+lines!(ax, ϵs, θc.(ϵs))
+f
 ##
-png(p,"Plots/Morse/critical_time.png")
+save("Plots/Morse/critical_time.pdf", f)
--- Code/Systems/Morse/potential_plot.jl
+++ Code/Systems/Morse/potential_plot.jl
@@ -1,14 +1,12 @@
-using Plots,LaTeXStrings
-
-default()
-default(label=false,width=4, markersize = 4, msw=0, 
-palette=:Set1_3, tickfontsize=14,
-guidefontsize=16, fontfamily="Computer Modern",dpi=1000,grid=false,framestyle = :box)
+include("../../plot_config.jl")
 
 V(q) = (1-exp(-q))^2
 
 qs = LinRange(-.8,4,1024)
-p = plot(qs,V.(qs),xlabel=L"a(r-r_e)",ylabel=L"V",dpi=300,
-yticks=([0,.5,1,1.5],[L"0",L"D/2",L"D",L"3D/2"]))
+f = Figure()
+ax = Axis(f[1, 1], xticks=0:0.2:1, xlabel=L"q", ylabel=L"V(q)", 
+yticks = (0:0.5:1.5, [L"0", L"D/2", L"D", L"3D/2"]))
+lines!(ax, ϵs, V.(qs))
+f
 ##
-png(p,"Plots/Morse/morse_potential.png")
+save("Plots/Morse/morse_potential.pdf", f)
