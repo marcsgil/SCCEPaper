fd3a4cdaa72a1f194747e86940481b71bff36b93
Marcos Gil <marcosgildeoliveira@gmail.com>
2023-07-28 17:49:15 -0300

    included plot that alfredo asked

--- Code/Systems/Kerr/kerr.jl
+++ Code/Systems/Kerr/kerr.jl
@@ -31,3 +31,22 @@ for χ ∈ (0.1,0.3,0.6,1.)
 
     jldsave("Results/Kerr/heat_χ=$χ.jld2";scatter_θs,line_θs,χ,quantum,sc,classical)
 end
+##
+χ = .5
+Us_sc = sc_energy(line_θs,χ)
+cs_sc = sc_heat(line_θs,χ)
+
+U2_sc = @. cs_sc / ( cs_sc + scatter_θs^2 * Us_sc^2)
+Us_q = quantum_energy(line_θs,χ,Es)
+cs_q = quantum_heat(line_θs,χ,Es)
+
+U2_q = @. cs_q / ( cs_q + scatter_θs^2 * Us_q^2 )
+
+fig = Figure()
+ax = Axis(fig[1,1])
+lines!(ax,scatter_θs,U2_q, label = "Quantum")
+lines!(ax,scatter_θs,U2_sc, label = "SC")
+fig
+ax.xlabel = L"\theta"
+ax.ylabel = L"(<U^2> - <U>^2) / <U^2>"
+fig
